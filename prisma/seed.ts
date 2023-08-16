de2a4d72ecc44ed67c02e557f9833241e6964b3b
AlinaSyniavska <alina22syniavska@gmail.com>
2023-08-16 16:17:34 +0300

    Seeded the database with Faker

---
+++ prisma/seed.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+import { faker } from '@faker-js/faker';
+import * as dotenv from 'dotenv';
+
+const prisma = new PrismaClient();
+
+const fakerProduct = (): any => ({
+  productName: faker.commerce.product(),
+  productPrice: faker.number.int({ min: 1, max: 1000 }),
+  productDescription: faker.commerce.productDescription()
+});
+
+async function main() {
+  const fakerRounds = 10000;
+  dotenv.config();
+  console.log("Seeding...");
+/// --------- Users ---------------
+  for (let i = 0; i < fakerRounds; i++) {
+    await prisma.products.create({ data: fakerProduct() });
+  }
+}
+
+
+main()
+  .catch((e) => console.error(e))
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+
